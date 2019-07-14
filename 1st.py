5a83232ce38a7c467bb96418b157a5a588d34f0c
malkamary <malkamary@elm.sa>
2019-07-14 17:44:28 +0300

    updating the test to include the unittest support

--- 1st.py
+++ 1st.py
@@ -1,4 +1,6 @@
 import time
+import os
+import unittest
 import xlrd
 import urllib3
 import selenium
@@ -10,29 +12,58 @@ from selenium.webdriver.common.by import By
 
 
 
-try:
- driver = webdriver.Safari()
- driver.set_page_load_timeout(10)
- driver.get("http://www.newtours.demoaut.com")
- driver.fullscreen_window()
- driver.find_element_by_xpath("/html/body/div/table/tbody/tr/td[2]/table/tbody/tr[4]/td/table/tbody/tr/td[2]/table/tbody/tr[2]/td[3]/form/table/tbody/tr[4]/td/table/tbody/tr[2]/td[2]/input").send_keys("mercury")
- driver.find_element_by_name("password").send_keys("mercury")
- driver.find_element_by_name("login").click()
- WebDriverWait(driver,10).until(EC.presence_of_element_located((By.XPATH,"/html/body/div[1]/table/tbody/tr/td[2]/table/tbody/tr[4]/td/table/tbody/tr/td[2]/table/tbody/tr[1]/td")))
- time.sleep(5)
- element = driver.find_element_by_xpath(
-     "/html/body/div[1]/table/tbody/tr/td[2]/table/tbody/tr[4]/td/table/tbody/tr/td[2]/table/tbody/tr[1]/td/img")
- assert (element.get_attribute("src") == "http://newtours.demoaut.com/images/masts/mast_flightfinder.gif") ,"test passed"
+class NewToursTest(unittest.TestCase):
+  driver = webdriver
 
-# driver.implicitly_wait(1000)
-#  print(element.get_attribute("src"))
- driver.quit()
-except:
-    traceback.print_exc()
+  def setUp(self) :
+      if(os.name =='nt'):
+          self.driver=webdriver.Chrome("chromedriver.exe")
+      else:
+          self.driver=webdriver.Safari()
 
-    driver.close()
-    # driver.quit()
+  def test_newToursLogin(self):
+      driver=self.driver
+      driver.set_page_load_timeout(10)
+      driver.get("http://www.newtours.demoaut.com")
+      driver.maximize_window()
+      driver.find_element_by_xpath("/html/body/div/table/tbody/tr/td[2]/table/tbody/tr[4]/td/table/tbody/tr/td[2]/table/tbody/tr[2]/td[3]/form/table/tbody/tr[4]/td/table/tbody/tr[2]/td[2]/input").send_keys("mercury")
+      driver.find_element_by_name("password").send_keys("mercury")
+      driver.find_element_by_name("login").click()
+      WebDriverWait(driver,10).until(EC.presence_of_element_located((By.XPATH,"/html/body/div[1]/table/tbody/tr/td[2]/table/tbody/tr[4]/td/table/tbody/tr/td[2]/table/tbody/tr[1]/td")))
+      time.sleep(5)
+      element = driver.find_element_by_xpath(
+          "/html/body/div[1]/table/tbody/tr/td[2]/table/tbody/tr[4]/td/table/tbody/tr/td[2]/table/tbody/tr[1]/td/img")
+      assert (element.get_attribute("src") == "http://newtours.demoaut.com/images/masts/mast_flightfinder.gif") ,"test failed"
+      driver.close()
 
-else:
-    pass
+  def test_newToursLoginf(self):
+          driver = self.driver
+          driver.set_page_load_timeout(10)
+          driver.get("http://www.newtours.demoaut.com")
+          driver.maximize_window()
+          driver.find_element_by_xpath(
+              "/html/body/div/table/tbody/tr/td[2]/table/tbody/tr[4]/td/table/tbody/tr/td[2]/table/tbody/tr[2]/td[3]/form/table/tbody/tr[4]/td/table/tbody/tr[2]/td[2]/input").send_keys(
+              "mercury")
+          driver.find_element_by_name("password").send_keys("mercury1")
+          driver.find_element_by_name("login").click()
+          WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH,
+                                                                          "/html/body/div[1]/table/tbody/tr/td[2]/table/tbody/tr[4]/td/table/tbody/tr/td[2]/table/tbody/tr[1]/td")))
+          time.sleep(5)
+          element = driver.find_element_by_xpath(
+              "/html/body/div[1]/table/tbody/tr/td[2]/table/tbody/tr[4]/td/table/tbody/tr/td[2]/table/tbody/tr[1]/td/img")
+          assert (element.get_attribute(
+              "src") == "http://newtours.demoaut.com/images/masts/mast_flightfinder.gif"), "test failed"
+          driver.close()
+
+  def tearDown(self):
+       self.driver.quit()
+    #  except:
+    #    traceback.print_exc()
+    #    driver.close()
+    # # driver.quit()
+    #
+    #  else:
+    #     pass
+if __name__=="__main__":
+    unittest.main()
 
